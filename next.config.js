5e365086382020c02b3d4f6092c6ed800f7c53e5
Your Name <you@example.com>
2026-03-26 19:40:12 +0700

    chore: upgrade next to 16

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    root: path.resolve(__dirname, './'),
+  },
 };
 
 module.exports = nextConfig;
